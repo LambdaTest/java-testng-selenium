5744d91a3dc4c95b99193e97156d0732b13c4e5f
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2021-08-03 12:10:22 +0000

    real mobile changes

---
+++ src/test/java/com/lambdatest/TestNGTodoMobile.java
@@ -0,0 +1,106 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTodoMobile {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        ;
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Google Pixel 4a");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", m.getName() + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        caps.setCapability("isRealMobile", true);
+
+        String[] Tags = new String[] { "Feature", "Tag", "Moderate" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+        String spanText;
+        System.out.println("Loading Url");
+        Thread.sleep(100);
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li1")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li2")).click();
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li3")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li4")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
+        driver.findElement(By.id("addbutton")).click();
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
+        driver.findElement(By.id("addbutton")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li1")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li3")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li7")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li8")).click();
+
+        System.out.println("Entering Text");
+        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
+
+        driver.findElement(By.id("addbutton")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li9")).click();
+
+        // Let's also assert that the todo we added is present in the list.
+
+        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
+        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
+        Status = "passed";
+        Thread.sleep(800);
+
+        System.out.println("TestFinished");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
