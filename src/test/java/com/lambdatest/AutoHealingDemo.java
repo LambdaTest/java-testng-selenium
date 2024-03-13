ce3e1c2a2813b68445adf24bc48134b0702244eb
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2024-03-13 13:11:13 +0000

    added failed case

--- src/test/java/com/lambdatest/AutoHealingDemo.java
+++ src/test/java/com/lambdatest/AutoHealingDemo.java
@@ -37,7 +37,7 @@ public class AutoHealingDemo
         // capabilities.setCapability("network",true);
         // capabilities.setCapability("console",true);
         capabilities.setCapability("visual",true);
-        capabilities.setCapability("autoHeal", true);
+        capabilities.setCapability("autoHeal", System.getProperty("autoheal", "true"));
 
         try
         {
@@ -72,9 +72,42 @@ public class AutoHealingDemo
     
             WebElement login = driver.findElementByXPath("//*[contains(text(), 'Submit')]");
             login.click();
+            System.setProperty("autoheal", "false");
 
         } catch (Exception e) {
             status = "failed";
+            System.setProperty("autoheal", "false");
+        }
+    }
+
+    @Test()
+    public void autoHealFalseWithChangedDOM() throws InterruptedException
+    {
+
+        try {
+            
+
+            driver.get("https://www.lambdatest.com/selenium-playground/auto-healing");
+            Thread.sleep(5000);
+
+            driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"AutoHealWithoutDomChanged-Login Case\", \"level\": \"info\"}}");
+    
+            WebElement changedom = driver.findElementByXPath("//*[contains(text(), 'Change DOM ID')]");
+            changedom.click();      //Uncomment this line in the 2nd test run for the autoheal to work. 
+    
+            WebElement username = driver.findElementById("username");
+            username.sendKeys("test@gmail.com");
+        
+            WebElement password = driver.findElementById("password");
+            password.sendKeys("password");
+    
+            WebElement login = driver.findElementByXPath("//*[contains(text(), 'Submit')]");
+            login.click();
+            System.setProperty("autoheal", "true");
+
+        } catch (Exception e) {
+            status = "failed";
+            System.setProperty("autoheal", "true");
         }
     }
 
@@ -100,6 +133,7 @@ public class AutoHealingDemo
     
             WebElement login = driver.findElementByXPath("//*[contains(text(), 'Submit')]");
             login.click();
+            status="passed";
 
         } catch (Exception e) {
             status = "failed";
