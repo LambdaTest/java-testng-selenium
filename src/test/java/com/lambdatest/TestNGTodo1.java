161462295f56a8454ecfe5539799db3ecd41357b
aryanc <aryanc@lambdatest.com>
2025-11-04 15:38:00 +0530

    updated dependencies and made changes as per selenium 4

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -5,7 +5,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.ITestContext;
@@ -22,28 +23,32 @@ public class TestNGTodo1 {
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
         String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
-        ;
+
         String hub = "@hub.lambdatest.com/wd/hub";
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", "MacOS Catalina");
-        caps.setCapability("browserName", "Safari");
-        caps.setCapability("version", "latest");
-        caps.setCapability("build", "TestNG With Java");
-        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
-        caps.setCapability("plugin", "git-testng");
+        // ✅ Use LambdaTest W3C-compliant structure (LT:Options)
+        MutableCapabilities ltOptions = new MutableCapabilities();
+        ltOptions.setCapability("build", "TestNG With Java");
+        ltOptions.setCapability("name", m.getName() + " - " + this.getClass().getName());
+        ltOptions.setCapability("platformName", "macOS Sonoma"); // modern macOS
+        ltOptions.setCapability("plugin", "git-testng");
+        ltOptions.setCapability("tags", new String[] { "Feature", "Falcon", "Severe" });
 
-        String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
+        /*
+        Enable Smart UI Project (optional)
+        ltOptions.setCapability("smartUI.project", "<Project Name>");
+        */
 
-        caps.setCapability("tags", Tags);
+        // ✅ Safari browser setup (works with Selenium 4.x)
+        SafariOptions browserOptions = new SafariOptions();
+        browserOptions.setCapability("browserVersion", "latest");
+        browserOptions.setCapability("LT:Options", ltOptions);
 
-        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
-
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), browserOptions);
     }
 
     @Test
     public void basicTest() throws InterruptedException {
-        String spanText;
         System.out.println("Loading Url");
         driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Opening WebApp\", \"level\": \"info\"}}");
 
@@ -51,16 +56,10 @@ public class TestNGTodo1 {
 
         driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Checking List Items\", \"level\": \"info\"}}");
 
-        System.out.println("Checking Box");
+        System.out.println("Checking Boxes...");
         driver.findElement(By.name("li1")).click();
-
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li2")).click();
-
-        System.out.println("Checking Box");
         driver.findElement(By.name("li3")).click();
-
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li4")).click();
 
         driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Adding Items\", \"level\": \"info\"}}");
@@ -76,44 +75,36 @@ public class TestNGTodo1 {
 
         driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Checking More Items\", \"level\": \"info\"}}");
 
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li1")).click();
-
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li3")).click();
-
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li7")).click();
-
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li8")).click();
         Thread.sleep(300);
 
         driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Adding and Verify List Items\", \"level\": \"info\"}}");
-        System.out.println("Entering Text");
         driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
-
         driver.findElement(By.id("addbutton")).click();
 
-        System.out.println("Checking Another Box");
         driver.findElement(By.name("li9")).click();
 
-        // Let's also assert that the todo we added is present in the list.
+        String spanText = driver.findElement(By.xpath("//li[9]/span")).getText();
+        Assert.assertEquals(spanText.trim(), "Get Taste of Lambda and Stick to It");
 
-        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
-        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
         Status = "passed";
         Thread.sleep(150);
 
-        System.out.println("TestFinished");
-
+        System.out.println("Test Finished");
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Adding Test Result and Closing Browser\", \"level\": \"info\"}}");
-        driver.executeScript("lambda-status=" + Status);
-        driver.quit();
+        try {
+            driver.executeScript("lambdatest_executor: {\"action\": \"stepcontext\", \"arguments\": {\"data\": \"Adding Test Result and Closing Browser\", \"level\": \"info\"}}");
+            driver.executeScript("lambda-status=" + Status);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
     }
-
 }
